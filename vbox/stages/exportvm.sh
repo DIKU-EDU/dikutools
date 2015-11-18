bd3ca6e3dd702edec814d09819d0e83b08969a36
Oleksandr Shturmov <oleks@oleks.info>
2015-11-18 16:36:21 +0100

    the actual script used to generate ov15-v1.0

--- vbox/stages/exportvm.sh
+++ vbox/stages/exportvm.sh
@@ -4,31 +4,29 @@
 set -e
 set -x
 
-HOST_PORT=3022
-
 # Check that the original VM actually exists
 vboxmanage showvminfo "$NAME" &> /dev/null
 
-FULL_NAME="${NAME}-v${VERSION}"
-OVA="${NAME}-v${VERSION}.ova"
+VERNAME="${NAME}-v${VERSION}"
+OVA_PATH="${NAME}-v${VERSION}.ova"
 
 # Shut down the new VM if it already exists and is running:
-if vboxmanage list runningvms | grep "\"${FULL_NAME}\"" > /dev/null ; then
-  vboxmanage controlvm "${FULL_NAME}" acpipowerbutton
+if vboxmanage list runningvms | grep "\"${VERNAME}\"" > /dev/null ; then
+  vboxmanage controlvm "${VERNAME}" acpipowerbutton
 
-  while vboxmanage list runningvms | grep "\"${FULL_NAME}\"" > /dev/null ; do 
+  while vboxmanage list runningvms | grep "\"${VERNAME}\"" > /dev/null ; do 
     sleep 30
   done
 fi
 
 # Delete the VM if it already exists:
-vboxmanage unregistervm "${FULL_NAME}" --delete &>/dev/null || true
+vboxmanage unregistervm "${VERNAME}" --delete &>/dev/null || true
 
 # Prepare a local folder (using tmp might not work, you need much space).
-mkdir -p "${FULL_NAME}"
-rm -rf "${FULL_NAME}/*"
+mkdir -p "${VERNAME}"
+rm -rf "${VERNAME}/*"
 
-FULL_PATH="${FULL_NAME}/${FULL_NAME}"
+FULL_PATH="${VERNAME}/${VERNAME}"
 
 CFG_PATH=$(vboxmanage showvminfo "$NAME" | grep "Config file" | \
   sed -E 's/Config file:\s*(.*?)$/\1/')
@@ -44,43 +42,46 @@ vboxmanage closemedium "${VDI_PATH}" --delete &>/dev/null || true
 vboxmanage clonehd "${HD_UUID}" "${VDI_PATH}" --format VDI
 
 # Create a new VM with the version number:
-vboxmanage createvm --name "${FULL_NAME}" --ostype "Debian_64" --register
+vboxmanage createvm --name "${VERNAME}" --ostype "Debian_64" --register
 
 # Create a SATA storage controller:
-vboxmanage storagectl "${FULL_NAME}" \
+vboxmanage storagectl "${VERNAME}" \
   --name "SATA Controller" --add sata \
   --controller IntelAHCI --portcount 1 --bootable on
 
 # Attach the VDI to this controller:
-vboxmanage storageattach "${FULL_NAME}" \
+vboxmanage storageattach "${VERNAME}" \
   --storagectl "SATA Controller" --port 0 \
   --device 0 --type hdd --medium "${VDI_PATH}"
 
 # Enable things like APIC shutdown:
-vboxmanage modifyvm "${FULL_NAME}" --ioapic on
-vboxmanage modifyvm "${FULL_NAME}" --memory 512 --vram 128
+vboxmanage modifyvm "${VERNAME}" --ioapic on
+vboxmanage modifyvm "${VERNAME}" --memory 512 --vram 128
 
 # Make it user-friendly
-vboxmanage modifyvm "${FULL_NAME}" --clipboard bidirectional
+vboxmanage modifyvm "${VERNAME}" --clipboard bidirectional
 
 # Disable USB. Otherwise this might happen:
 # https://www.virtualbox.org/ticket/14469
-vboxmanage modifyvm "${FULL_NAME}" --mouse ps2
-vboxmanage modifyvm "${FULL_NAME}" --usb off
+vboxmanage modifyvm "${VERNAME}" --mouse ps2
+vboxmanage modifyvm "${VERNAME}" --usb off
 
 # Enable SSH into the box, assuming openssh is already installed.
-VBoxManage modifyvm "${FULL_NAME}" --natpf1 "ssh,tcp,,${HOST_PORT},,22"
+VBoxManage modifyvm "${VERNAME}" \
+  --natpf1 "ssh,tcp,,${SSH_HOST_PORT},,22"
 
 # Start the VM and scrub it!
-vboxmanage startvm "${FULL_NAME}" --type headless
+vboxmanage startvm "${VERNAME}" --type headless
 
-while ! vboxmanage list runningvms | grep "\"${FULL_NAME}\"" > /dev/null ; do 
+while ! vboxmanage list runningvms | grep "\"${VERNAME}\"" > /dev/null ; do 
   sleep 30
 done
 
-sshpass -p "${PASSWD}" ssh -p ${HOST_PORT} \
+sshpass -p "${PASSWD}" ssh -p ${SSH_HOST_PORT} \
   -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
   root@localhost "bash -c \"
+adduser \"${USER}\" vboxsf
+
 rm -rf /var/cache/apt/*
 rm -rf /var/lib/apt/lists/*
 rm -rf /var/log/dpkg.log*
@@ -91,25 +92,32 @@ rm -rf /usr/lib/x86_64-linux-gnu/dri
 
 dd if=/dev/zero of=/tmp/zero bs=1M
 rm /tmp/zero
+shutdown -h now
 \""
 
 # Shut down the VM
-vboxmanage controlvm "${FULL_NAME}" acpipowerbutton
+vboxmanage controlvm "${VERNAME}" acpipowerbutton
 
-while vboxmanage list runningvms | grep "\"${FULL_NAME}\"" > /dev/null ; do 
+while vboxmanage list runningvms | grep "\"${VERNAME}\"" > /dev/null ; do 
   sleep 30
 done
 
 # Finally, compact the disk after the clean up:
 vboxmanage modifyhd "${VDI_PATH}" --compact
 
+rm -rf "${OVA_PATH}"
+
 # And export the OVA.
-vboxmanage export "${FULL_NAME}" -o "${OVA}" --vsys 0 \
-  --product "${FULL_NAME}" \
-  --version "${VERSION}" \
-  --vendor "DIKU" --vendorurl "http://www.diku.dk/"
+vboxmanage export "${VERNAME}" \
+  --output "${OVA_PATH}" \
+  --vsys 0 \
+    --product "${LONG_NAME}" \
+    --description "${DESCRIPTION}" \
+    --version "${VERSION}" \
+    --vendor "DIKU" \
+    --vendorurl "http://www.diku.dk/"
 
 # Clean up
-vboxmanage unregistervm "${FULL_NAME}" --delete
+vboxmanage unregistervm "${VERNAME}" --delete
 # ^^^ That should delete the disk from media manager as well.
-rm -rf "${FULL_NAME}"
+rm -rf "${VERNAME}"
