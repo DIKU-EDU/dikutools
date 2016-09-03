e0188d9b095d3fead504663606bc8ac2fb104df5
oleks <oleks@oleks.info>
2016-09-03 15:31:06 +0200

    Parametrize the port as well

--- vm/headless.sh
+++ vm/headless.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 GEN_ARCH=x86_64
 MEM=512
 HDA=$1
+PORT=1337
 
 qemu-system-${GEN_ARCH} \
   -enable-kvm \
@@ -12,4 +13,4 @@ qemu-system-${GEN_ARCH} \
   -m ${MEM} \
   -hda "${HDA}" \
   -net nic \
-  -net user,hostfwd=tcp::1337-:22
+  -net user,hostfwd=tcp::${PORT}-:22
