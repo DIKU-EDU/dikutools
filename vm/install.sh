8ae73a76d1d5b1aa6f23a42a25f52d185468eca2
oleks <oleks@oleks.info>
2016-09-05 00:10:18 +0200

    Install nginx as well

---
+++ vm/install.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p sshpass
+
+set -euo pipefail
+
+PORT=1337
+
+sshpass -p "hamster2" ssh \
+  -p ${PORT} \
+  -o UserKnownHostsFile=/dev/null \
+  -o StrictHostKeyChecking=no \
+  -o LogLevel=ERROR \
+  root@localhost "bash -c \"
+apt-get update
+apt-get -y install git xorg build-essential gdb qemu bochs nginx
+cat <<EOF | tr '%' '\\$' > /etc/nginx/sites-available/public_html
+server {
+  listen 80;
+  listen [::]:80;
+
+  server_name localhost;
+
+  index index.html;
+
+  location ~ \\\"^/~([a-z]{3}[0-9]{3})(/.*?)?$\\\" {
+    alias /home/%1/public_html/%2;
+    autoindex on;
+  }
+}
+EOF
+cd /etc/nginx/sites-enabled/
+rm -f public_html
+ln -s ../sites-available/public_html public_html
+service nginx restart
+\""
