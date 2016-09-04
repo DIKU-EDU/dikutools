7337d3b226f167dcc0eb92b3e67e0826b9ed72b7
oleks <oleks@oleks.info>
2016-09-04 08:28:39 +0200

    A welcome message script

---
+++ vm/welcome.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p sshpass
+
+set -euo pipefail
+
+MESSAGE=$1
+PORT=1337
+
+sshpass -p "hamster2" ssh \
+  -p ${PORT} \
+  -o UserKnownHostsFile=/dev/null \
+  -o StrictHostKeyChecking=no \
+  -o LogLevel=ERROR \
+  root@localhost "bash -c \"
+apt-get update
+apt-get -y install toilet
+echo $MESSAGE | toilet > /etc/motd
+apt-get remove --auto-remove toilet
+\""
