8a544c0625ba0786e9e51bbda9ee1be544bd1015
oleks <oleks@oleks.info>
2016-09-05 01:14:30 +0200

    Install it with GUI

--- vm/y86_64.sh
+++ vm/y86_64.sh
@@ -15,6 +15,16 @@ rm -rf sim
 wget -N http://csapp.cs.cmu.edu/3e/sim.tar
 tar xvf sim.tar
 cd sim
+patch Makefile <<EOF
+3c3
+< #GUIMODE=-DHAS_GUI
+---
+> GUIMODE=-DHAS_GUI
+10c10
+< TKLIBS=-L/usr/lib -ltk -ltcl
+---
+> TKLIBS=-L/usr/lib -ltk8.5 -ltcl8.5
+EOF
 make clean
 make
 cd ..
